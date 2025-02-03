1e96cbdf009c3bdfc064497d17e39e24dd08908f
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-02-03 10:40:53 +0700

    update Product entities

--- OverShopping/Models/Entities/Product.cs
+++ OverShopping/Models/Entities/Product.cs
@@ -3,9 +3,23 @@
     public class Product
     {
         public int productId { get; set; }
-        public string productName { get; set; }
-        public string productDescription { get; set; }
+        public required string productName { get; set; }
+        public string? productDescription { get; set; }
+        public string? productImage { get; set; }
         public decimal productPrice { get; set; }
+        public int productStock { get; set; }
+        public int productSold { get; set; }
+        public int totalReview { get; set; }
+        public decimal productRating { get; set; }
         public int category_id { get; set; }
+        public DateTime created_at { get; set; }
+
+        public ICollection<Review>? reviews { get; set; }
+        public ICollection<Cart>? carts { get; set; }
+        public ICollection<Order>? orders { get; set; }
+        public ICollection<Transaction>? transactions { get; set; }
+        public ICollection<SearchHistory>? searchHistory { get; set; }
+        public ICollection<ActivityHistory>? activityHistory { get; set; }
+
     }
 }
